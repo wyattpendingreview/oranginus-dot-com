ce3d1e6008da641997b31b5453d2b2d79b172762
wiz <wyatt.pendingreview@gmail.com>
2026-03-24 12:58:47 -0700

    fix build

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html',
 			precompress: false,
-			strict: true
+			strict: false
 		})
 	},
 	vitePlugin: {
