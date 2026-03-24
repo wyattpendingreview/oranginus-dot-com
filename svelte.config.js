00b68300739a644c150a72ba7977da075652fb56
wiz <wyatt.pendingreview@gmail.com>
2026-03-24 11:00:51 -0700

    initial commit - svelte boilerplate

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	},
+	vitePlugin: {
+		dynamicCompileOptions: ({ filename }) =>
+			filename.includes('node_modules') ? undefined : { runes: true }
+	}
+};
+
+export default config;
