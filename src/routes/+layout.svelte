7f70e4b789cf3a86aece21a2b75ddbc0c7575a46
wiz <wyatt.pendingreview@gmail.com>
2026-03-25 08:58:24 -0700

    add audio

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,19 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+	let audioPlaying = $state(false);
+
+	const playAudio = (event: MouseEvent) => {
+		const doc = (event.target as HTMLElement).ownerDocument;
+		const audio = doc.querySelector('.audio-control > audio') as HTMLAudioElement;
+
+		if (audio.paused) {
+			audio.play();
+		} else {
+			audio.pause();
+		}
+		audioPlaying = !audio.paused;
+	};
 </script>
 
 <svelte:head>
@@ -9,6 +22,17 @@
 </svelte:head>
 
 {@render children()}
+
+<div class="audio-control">
+	<audio autoplay loop>
+		<source
+			src="/audio/drone-low-pitched-rotating-growling-ambient-background-effect.mp3"
+			type="audio/mpeg"
+		/>
+	</audio>
+
+	<button onclick={playAudio} aria-label="Toggle audio" class:on={audioPlaying}></button>
+</div>
 
 <style lang="scss">
 	:global {
@@ -46,6 +70,27 @@
 				2px 0 2px #000000,
 				0 -2px 2px #000000,
 				0 2px 2px #000000;
+		}
+	}
+
+	.audio-control {
+		position: fixed;
+		bottom: 12px;
+		right: 12px;
+
+		button {
+			width: 70px;
+			height: 71px;
+
+			border: 4px solid #646464;
+			background: url('/img/off.png');
+			background-size: contain;
+			cursor: pointer;
+
+			&.on {
+				background: url('/img/on.png');
+				background-size: contain;
+			}
 		}
 	}
 </style>
